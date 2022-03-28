5e7206cab776df0d36e08c9c1753748c95ce9b8a
Emanuel <emanuel10.01@gmail.com>
2022-03-28 20:28:28 -0300

    Implementação da função que determina o tipo do grafo (euleriano, semi-euleriano ou não euleriano

--- Trabalho3/main.py
+++ Trabalho3/main.py
@@ -3,6 +3,25 @@ import json
 import sys
 
 
+def is_euler(g):
+    odd_vertices = 0
+
+    for v in g:
+        degree = g.vertice_density(v)
+        if degree % 2 != 0:
+            odd_vertices += 1
+    
+    if odd_vertices == 0:
+        return "Euleriano"
+
+    if odd_vertices == 2:
+        return "Semi-euleriano"
+    
+    if odd_vertices > 2:
+        return "Não euleriano"
+
+    return "Error: Entrada inválida"
+
 def fleury(graph):
     g = graph   # Cópia do grafo
 
